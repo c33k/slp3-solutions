16091231a5359775ecc7c7c3879ca2b082cf99f7
c33k <rltoscano@gmail.com>
2018-11-17 18:39:39 +0100

    Chapter 2: solution of exercises 2.1.2 and 2.1.3

--- javascript/_specs/chapter-2-spec.js
+++ javascript/_specs/chapter-2-spec.js
@@ -2,8 +2,6 @@ const test = require("tape");
 const NLP2 = require("../chapter-2");
 
 test("2.1.1 - set of all alphabetic strings", t => {
-  t.plan(5);
-
   t.ok(NLP2.regexAlphabetic('aiygiyagsjaywgdj'));
   t.ok(NLP2.regexAlphabetic(''));
   t.notOk(NLP2.regexAlphabetic('asdjkhsdk askdjhdkjh'), 'whitespace is non-alphabetic');
@@ -12,3 +10,32 @@ test("2.1.1 - set of all alphabetic strings", t => {
 
   t.end();
 });
+
+test("2.1.2 - lowercase alphabetic strings ending in b", t => {
+    t.ok(NLP2.regexLowerCaseEndingInB('aaaaaab'));
+    t.ok(NLP2.regexLowerCaseEndingInB('b'));
+    t.ok(NLP2.regexLowerCaseEndingInB('abcdefghib'));
+    t.notOk(NLP2.regexLowerCaseEndingInB('ASDAdadsdb'), 'uppercase letters are not allowed');
+    t.notOk(NLP2.regexLowerCaseEndingInB('asdddB'), 'uppercase B is not allowed');
+    t.notOk(NLP2.regexLowerCaseEndingInB(''), 'empty string is not in this set');
+    t.notOk(NLP2.regexLowerCaseEndingInB('12313b'), 'numbers are not valid');
+    t.notOk(NLP2.regexLowerCaseEndingInB('asdjkhsdk askdjhdkjhb'), 'whitespace is non-alphabetic');
+
+    t.end();
+  });
+
+  test("2.1.3 - alphabet a,b with all a surrounded by bs", t => {
+    t.ok(NLP2.testASurroundedByB('bab'));
+    t.ok(NLP2.testASurroundedByB('babababab'));
+    t.ok(NLP2.testASurroundedByB('bbbab'), 'number of bs not limited');
+    t.notOk(NLP2.testASurroundedByB('baba'), 'baba - last a not surrounded by b');
+    t.ok(NLP2.testASurroundedByB('bb'), 'can have only bs');
+    t.ok(NLP2.testASurroundedByB('b'), 'can have only one b');
+    t.ok(NLP2.testASurroundedByB(''), 'empty string is valid');
+    t.notOk(NLP2.testASurroundedByB('babc'), 'c is not in the alphabet');
+    t.notOk(NLP2.testASurroundedByB('bab bab'), 'white space not allowed');
+    t.end();
+  });
+
+
+  
--- javascript/chapter-2.js
+++ javascript/chapter-2.js
@@ -1,1 +1,11 @@
+//2.1-1: set of all alphabetic strings
 module.exports.regexAlphabetic = string => /^[A-Za-z]*$/.test(string);
+
+//2.1-2: set of lowercase alphabetic strings ending in b
+module.exports.regexLowerCaseEndingInB = string => /^[a-z]*b$/.test(string);
+
+/*2.1-3: the set of all strings from the alphabet 
+  a,b such that each a is immediately preceded by and 
+  immediately followed by a b;
+*/
+module.exports.testASurroundedByB = string => /^(b(ab)*)*$/.test(string);
