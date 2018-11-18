0249b16dd4439d675e23959742c7d8621a7ba792
c33k <rltoscano@gmail.com>
2018-11-18 22:32:54 +0100

    Chapter 2: solution of exercises 2.2-3 and 2.2-4

--- javascript/_specs/chapter-2-spec.js
+++ javascript/_specs/chapter-2-spec.js
@@ -24,40 +24,63 @@ test("2.1.2 - lowercase alphabetic strings ending in b", t => {
     t.end();
   });
 
-  test("2.1.3 - alphabet a,b with all a surrounded by bs", t => {
-    t.ok(NLP2.testASurroundedByB('bab'));
-    t.ok(NLP2.testASurroundedByB('babababab'));
-    t.ok(NLP2.testASurroundedByB('bbbab'), 'number of bs not limited');
-    t.notOk(NLP2.testASurroundedByB('baba'), 'baba - last a not surrounded by b');
-    t.ok(NLP2.testASurroundedByB('bb'), 'can have only bs');
-    t.ok(NLP2.testASurroundedByB('b'), 'can have only one b');
-    t.ok(NLP2.testASurroundedByB(''), 'empty string is valid');
-    t.notOk(NLP2.testASurroundedByB('babc'), 'c is not in the alphabet');
-    t.notOk(NLP2.testASurroundedByB('bab bab'), 'white space not allowed');
+test("2.1.3 - alphabet a,b with all a surrounded by bs", t => {
+  t.ok(NLP2.testASurroundedByB('bab'));
+  t.ok(NLP2.testASurroundedByB('babababab'));
+  t.ok(NLP2.testASurroundedByB('bbbab'), 'number of bs not limited');
+  t.notOk(NLP2.testASurroundedByB('baba'), 'baba - last a not surrounded by b');
+  t.ok(NLP2.testASurroundedByB('bb'), 'can have only bs');
+  t.ok(NLP2.testASurroundedByB('b'), 'can have only one b');
+  t.ok(NLP2.testASurroundedByB(''), 'empty string is valid');
+  t.notOk(NLP2.testASurroundedByB('babc'), 'c is not in the alphabet');
+  t.notOk(NLP2.testASurroundedByB('bab bab'), 'white space not allowed');
+  t.end();
+});
+
+test("2.2.1 - the set of all strings with two consecutive repeated words", t => {
+  t.ok(NLP2.consecutiveRepeatedWords('the the'));
+  t.ok(NLP2.consecutiveRepeatedWords('Humbert Humbert'));
+  t.ok(NLP2.consecutiveRepeatedWords('123 123'));
+  t.notOk(NLP2.consecutiveRepeatedWords('the something'));
+  t.ok(NLP2.consecutiveRepeatedWords('the the something'));
+  t.notOk(NLP2.consecutiveRepeatedWords(''))
+  t.end();
+});
+
+test("2.2.2 - begins with integer and ends with word", t => {
+  t.ok(NLP2.startsWithIntegerEndsWithWord('123123 something 1hi1h23i Word'));
+  t.notOk(NLP2.startsWithIntegerEndsWithWord('Word word'));
+  t.notOk(NLP2.startsWithIntegerEndsWithWord('word 123'));
+  t.notOk(NLP2.startsWithIntegerEndsWithWord('123'));
+  t.notOk(NLP2.startsWithIntegerEndsWithWord('word'));
+  t.notOk(NLP2.startsWithIntegerEndsWithWord(''));
+  t.end();
+});
+
+test("2.2.3 - has both words grotto and raven", t => {
+  t.ok(NLP2.grottoRaven('grotto raven'));
+  t.ok(NLP2.grottoRaven('raven grotto'));
+  t.ok(NLP2.grottoRaven('some other raven words grotto'));
+  t.notOk(NLP2.grottoRaven('grottos ravens'));
+  t.notOk(NLP2.grottoRaven('grotto ravens'), 'has grotto but dont have raven');
+  t.notOk(NLP2.grottoRaven(''));
+  t.end();
+});
+
+test("2.2.4 - english words in the beginning of a sentence added to a register", 
+  t => {
+    let str = 'The Earth is beautiful. Rafael loves Regex';    
+    let matches = NLP2.regex224.exec(str);
+    t.equal(matches[1], 'The');
+    
+    matches = NLP2.regex224.exec(str);
+    t.equal(matches[1], 'Rafael');    
+    
+    matches = NLP2.regex224.exec(str);
+    t.equal(matches, null);
     t.end();
   });
 
-  test("2.2.1 - the set of all strings with two consecutive repeated words", t => {
-    t.ok(NLP2.consecutiveRepeatedWords('the the'));
-    t.ok(NLP2.consecutiveRepeatedWords('Humbert Humbert'));
-    t.ok(NLP2.consecutiveRepeatedWords('123 123'));
-    t.notOk(NLP2.consecutiveRepeatedWords('the something'));
-    t.ok(NLP2.consecutiveRepeatedWords('the the something'));
-    t.notOk(NLP2.consecutiveRepeatedWords(''))
-    t.end();
-  });
-
-  test("2.2.2 - begins with integer and ends with word", t => {
-    t.ok(NLP2.startsWithIntegerEndsWithWord('123123 something 1hi1h23i Word'));
-    t.notOk(NLP2.startsWithIntegerEndsWithWord('Word word'));
-    t.notOk(NLP2.startsWithIntegerEndsWithWord('word 123'));
-    t.notOk(NLP2.startsWithIntegerEndsWithWord('123'));
-    t.notOk(NLP2.startsWithIntegerEndsWithWord('word'));
-    t.notOk(NLP2.startsWithIntegerEndsWithWord(''));
-    t.end();
-  });
-
-  
 
 
   
--- javascript/chapter-2.js
+++ javascript/chapter-2.js
@@ -14,11 +14,34 @@ module.exports.testASurroundedByB = string => /^(b(ab)*)*$/.test(string);
     repeated words (e.g., “Humbert Humbert” and “the the” 
     but not “the bug” or “the big bug”);
 */
-module.exports.consecutiveRepeatedWords = string => /\b(\w+)\s(\1)\b/.test(string);
+module.exports.consecutiveRepeatedWords = string => 
+  /\b(\w+)\s(\1)\b/.test(string);
 
 /*2.2-2: all strings that start at the beginning of the line 
   with an integer and that end at the end of the line with a word;
   */
-
  module.exports.startsWithIntegerEndsWithWord = 
     string => /^\d+.*\b\w+$/.test(string);
+
+/*2.2-3: all strings that have both the word grotto and the word raven in them 
+ * (but not, e.g., words like grottos that merely contain the word grotto);*/
+
+module.exports.grottoRaven = string => 
+  /^(?=.*\bgrotto\b)(?=.*\braven\b).*$/g.test(string);
+
+/*2.2-4: write a pattern that places the first word of an English sentence in a 
+  * register. Deal with punctuation.
+  *
+  * obs.: that means: we can't simply match the beginning of the string. The
+  * word "sentence" is the important detail here.
+  *
+  * Using the positive lookbehind feature from JavaScript's reges, the solution
+  * could be "(?<=^|[\.\!\?]\s)([A-Z]\w*) But let's discard this option in our 
+  * solution given the fact that this is a specific feature from the language
+  * and was not mentioned in the book.
+  *
+  * The parenthesis in the second part of the regex is important because, as
+  * the problem states, we need to add the match to a register.
+  */
+
+module.exports.regex224 = /(?:^|[\.\!\?]\s)([A-Z]\w*)/g;
