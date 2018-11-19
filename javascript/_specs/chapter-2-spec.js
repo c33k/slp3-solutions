2dd955f40366a23709e228b063d590e13dd8ee98
c33k <rltoscano@gmail.com>
2018-11-19 22:15:35 +0100

    Refactor: exports the regex itself instead of the result of a test

--- javascript/_specs/chapter-2-spec.js
+++ javascript/_specs/chapter-2-spec.js
@@ -2,68 +2,68 @@ const test = require("tape");
 const NLP2 = require("../chapter-2");
 
 test("2.1.1 - set of all alphabetic strings", t => {
-  t.ok(NLP2.regexAlphabetic('aiygiyagsjaywgdj'));
-  t.ok(NLP2.regexAlphabetic(''));
-  t.notOk(NLP2.regexAlphabetic('asdjkhsdk askdjhdkjh'), 'whitespace is non-alphabetic');
-  t.notOk(NLP2.regexAlphabetic('123415'), 'numbers are non-alphabetic');
-  t.notOk(NLP2.regexAlphabetic('adasd1'));
+  t.ok(NLP2.regex211.test('aiygiyagsjaywgdj'));
+  t.ok(NLP2.regex211.test(''));
+  t.notOk(NLP2.regex211.test('asdjkhsdk askdjhdkjh'), 'whitespace is non-alphabetic');
+  t.notOk(NLP2.regex211.test('123415'), 'numbers are non-alphabetic');
+  t.notOk(NLP2.regex211.test('adasd1'));
 
   t.end();
 });
 
 test("2.1.2 - lowercase alphabetic strings ending in b", t => {
-    t.ok(NLP2.regexLowerCaseEndingInB('aaaaaab'));
-    t.ok(NLP2.regexLowerCaseEndingInB('b'));
-    t.ok(NLP2.regexLowerCaseEndingInB('abcdefghib'));
-    t.notOk(NLP2.regexLowerCaseEndingInB('ASDAdadsdb'), 'uppercase letters are not allowed');
-    t.notOk(NLP2.regexLowerCaseEndingInB('asdddB'), 'uppercase B is not allowed');
-    t.notOk(NLP2.regexLowerCaseEndingInB(''), 'empty string is not in this set');
-    t.notOk(NLP2.regexLowerCaseEndingInB('12313b'), 'numbers are not valid');
-    t.notOk(NLP2.regexLowerCaseEndingInB('asdjkhsdk askdjhdkjhb'), 'whitespace is non-alphabetic');
+    t.ok(NLP2.regex212.test('aaaaaab'));
+    t.ok(NLP2.regex212.test('b'));
+    t.ok(NLP2.regex212.test('abcdefghib'));
+    t.notOk(NLP2.regex212.test('ASDAdadsdb'), 'uppercase letters are not allowed');
+    t.notOk(NLP2.regex212.test('asdddB'), 'uppercase B is not allowed');
+    t.notOk(NLP2.regex212.test(''), 'empty string is not in this set');
+    t.notOk(NLP2.regex212.test('12313b'), 'numbers are not valid');
+    t.notOk(NLP2.regex212.test('asdjkhsdk askdjhdkjhb'), 'whitespace is non-alphabetic');
 
     t.end();
   });
 
 test("2.1.3 - alphabet a,b with all a surrounded by bs", t => {
-  t.ok(NLP2.testASurroundedByB('bab'));
-  t.ok(NLP2.testASurroundedByB('babababab'));
-  t.ok(NLP2.testASurroundedByB('bbbab'), 'number of bs not limited');
-  t.notOk(NLP2.testASurroundedByB('baba'), 'baba - last a not surrounded by b');
-  t.ok(NLP2.testASurroundedByB('bb'), 'can have only bs');
-  t.ok(NLP2.testASurroundedByB('b'), 'can have only one b');
-  t.ok(NLP2.testASurroundedByB(''), 'empty string is valid');
-  t.notOk(NLP2.testASurroundedByB('babc'), 'c is not in the alphabet');
-  t.notOk(NLP2.testASurroundedByB('bab bab'), 'white space not allowed');
+  t.ok(NLP2.regex213.test('bab'));
+  t.ok(NLP2.regex213.test('babababab'));
+  t.ok(NLP2.regex213.test('bbbab'), 'number of bs not limited');
+  t.notOk(NLP2.regex213.test('baba'), 'baba - last a not surrounded by b');
+  t.ok(NLP2.regex213.test('bb'), 'can have only bs');
+  t.ok(NLP2.regex213.test('b'), 'can have only one b');
+  t.ok(NLP2.regex213.test(''), 'empty string is valid');
+  t.notOk(NLP2.regex213.test('babc'), 'c is not in the alphabet');
+  t.notOk(NLP2.regex213.test('bab bab'), 'white space not allowed');
   t.end();
 });
 
 test("2.2.1 - the set of all strings with two consecutive repeated words", t => {
-  t.ok(NLP2.consecutiveRepeatedWords('the the'));
-  t.ok(NLP2.consecutiveRepeatedWords('Humbert Humbert'));
-  t.ok(NLP2.consecutiveRepeatedWords('123 123'));
-  t.notOk(NLP2.consecutiveRepeatedWords('the something'));
-  t.ok(NLP2.consecutiveRepeatedWords('the the something'));
-  t.notOk(NLP2.consecutiveRepeatedWords(''))
+  t.ok(NLP2.regex221.test('the the'));
+  t.ok(NLP2.regex221.test('Humbert Humbert'));
+  t.ok(NLP2.regex221.test('123 123'));
+  t.notOk(NLP2.regex221.test('the something'));
+  t.ok(NLP2.regex221.test('the the something'));
+  t.notOk(NLP2.regex221.test(''))
   t.end();
 });
 
 test("2.2.2 - begins with integer and ends with word", t => {
-  t.ok(NLP2.startsWithIntegerEndsWithWord('123123 something 1hi1h23i Word'));
-  t.notOk(NLP2.startsWithIntegerEndsWithWord('Word word'));
-  t.notOk(NLP2.startsWithIntegerEndsWithWord('word 123'));
-  t.notOk(NLP2.startsWithIntegerEndsWithWord('123'));
-  t.notOk(NLP2.startsWithIntegerEndsWithWord('word'));
-  t.notOk(NLP2.startsWithIntegerEndsWithWord(''));
+  t.ok(NLP2.regex222.test('123123 something 1hi1h23i Word'));
+  t.notOk(NLP2.regex222.test('Word word'));
+  t.notOk(NLP2.regex222.test('word 123'));
+  t.notOk(NLP2.regex222.test('123'));
+  t.notOk(NLP2.regex222.test('word'));
+  t.notOk(NLP2.regex222.test(''));
   t.end();
 });
 
 test("2.2.3 - has both words grotto and raven", t => {
-  t.ok(NLP2.grottoRaven('grotto raven'));
-  t.ok(NLP2.grottoRaven('raven grotto'));
-  t.ok(NLP2.grottoRaven('some other raven words grotto'));
-  t.notOk(NLP2.grottoRaven('grottos ravens'));
-  t.notOk(NLP2.grottoRaven('grotto ravens'), 'has grotto but dont have raven');
-  t.notOk(NLP2.grottoRaven(''));
+  t.ok(NLP2.regex223.test('grotto raven'));
+  t.ok(NLP2.regex223.test('raven grotto'));
+  t.ok(NLP2.regex223.test('some other raven words grotto'));
+  t.notOk(NLP2.regex223.test('grottos ravens'));
+  t.notOk(NLP2.regex223.test('grotto ravens'), 'has grotto but dont have raven');
+  t.notOk(NLP2.regex223.test(''));
   t.end();
 });
 
--- javascript/chapter-2.js
+++ javascript/chapter-2.js
@@ -1,33 +1,30 @@
 //2.1-1: set of all alphabetic strings
-module.exports.regexAlphabetic = string => /^[A-Za-z]*$/.test(string);
+module.exports.regex211 = /^[A-Za-z]*$/;
 
 //2.1-2: set of lowercase alphabetic strings ending in b
-module.exports.regexLowerCaseEndingInB = string => /^[a-z]*b$/.test(string);
+module.exports.regex212 = /^[a-z]*b$/;
 
 /*2.1-3: the set of all strings from the alphabet 
   a,b such that each a is immediately preceded by and 
   immediately followed by a b;
 */
-module.exports.testASurroundedByB = string => /^(b(ab)*)*$/.test(string);
+module.exports.regex213 = /^(b(ab)*)*$/;
 
 /*2.2-1: the set of all strings with two consecutive 
     repeated words (e.g., “Humbert Humbert” and “the the” 
     but not “the bug” or “the big bug”);
 */
-module.exports.consecutiveRepeatedWords = string => 
-  /\b(\w+)\s(\1)\b/.test(string);
+module.exports.regex221 = /\b(\w+)\s(\1)\b/;
 
 /*2.2-2: all strings that start at the beginning of the line 
   with an integer and that end at the end of the line with a word;
   */
- module.exports.startsWithIntegerEndsWithWord = 
-    string => /^\d+.*\b\w+$/.test(string);
+ module.exports.regex222 = /^\d+.*\b\w+$/;
 
 /*2.2-3: all strings that have both the word grotto and the word raven in them 
  * (but not, e.g., words like grottos that merely contain the word grotto);*/
 
-module.exports.grottoRaven = string => 
-  /^(?=.*\bgrotto\b)(?=.*\braven\b).*$/g.test(string);
+module.exports.regex223 = /^(?=.*\bgrotto\b)(?=.*\braven\b).*$/;
 
 /*2.2-4: write a pattern that places the first word of an English sentence in a 
   * register. Deal with punctuation.
@@ -35,10 +32,11 @@ module.exports.grottoRaven = string =>
   * obs.: that means: we can't simply match the beginning of the string. The
   * word "sentence" is the important detail here.
   *
-  * Using the positive lookbehind feature from JavaScript's reges, the solution
+  * Using the positive lookbehind feature from JavaScript's regex, the solution
   * could be "(?<=^|[\.\!\?]\s)([A-Z]\w*) But let's discard this option in our 
   * solution given the fact that this is a specific feature from the language
-  * and was not mentioned in the book.
+  * and was not mentioned in the book. It also only works in some browsers and
+  * not in NodeJS.
   *
   * The parenthesis in the second part of the regex is important because, as
   * the problem states, we need to add the match to a register.
